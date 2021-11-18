d7227932886ca6693bdb0a7ad2d6616a54f9d651
arkarianh <maikel0hans@gmail.com>
2021-11-18 10:48:24 +0100

    actualizado++

--- src/components/PagPrincipal/Clasificacion.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Clasificacion() {
-    return (
-        <div>
-            <h1>Clasificacion</h1>
-        </div>
-    )
-}
--- src/components/PagPrincipal/Noticias.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Noticias() {
-    return (
-        <div>
-            <h1>Noticias</h1>
-        </div>
-    )
-}
